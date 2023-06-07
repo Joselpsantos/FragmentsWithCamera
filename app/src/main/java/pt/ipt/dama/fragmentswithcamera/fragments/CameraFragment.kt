236613a1f48f1cd9ebcf1de7e5266a3e730e7c7b
Joselpsantos <113514374+Joselpsantos@users.noreply.github.com>
2023-06-07 22:08:12 +0100

    Started code on gpsfragment

--- app/src/main/java/pt/ipt/dama/fragmentswithcamera/fragments/CameraFragment.kt
+++ app/src/main/java/pt/ipt/dama/fragmentswithcamera/fragments/CameraFragment.kt
@@ -1,30 +1,21 @@
 package pt.ipt.dama.fragmentswithcamera.fragments
 
-import android.Manifest
-import android.content.ContentValues
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import pt.ipt.dama.fragmentswithcamera.R
+
 
 class CameraFragment : Fragment() {
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.camerafragment, container, false)
+    }
 
 }
--- app/src/main/java/pt/ipt/dama/fragmentswithcamera/fragments/GpsFragment.kt
+++ app/src/main/java/pt/ipt/dama/fragmentswithcamera/fragments/GpsFragment.kt
@@ -1,6 +1,38 @@
 package pt.ipt.dama.fragmentswithcamera.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import pt.ipt.dama.fragmentswithcamera.R
 
 class GpsFragment : Fragment() {
+
+    private lateinit var textViewGPSPosition: TextView
+    private lateinit var bt: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.gpsfragment, container, false)
+        bt = view.findViewById(R.id.button)
+        bt.setOnClickListener{
+            getLocation()
+        }
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
 }
